8f6ba0af830edea290ed368942af9b1bd0e388e6
peters0908 <peters0908@gmail.com>
2022-11-23 14:05:07 +0800

    999

--- 專案3/datasource.py
+++ 專案3/datasource.py
@@ -1,15 +1,36 @@
 import requests
 
 api_key = "75f99c6e5bf613675e743a9c3ab32009"
-cityName = "Taipei"
 
-def get_forecast_data():
+tw_county_names = {"台北":"Taipei",
+                   "台中":"Taichung",
+                   "基隆":"Keelung",
+                   "台南":"Tainan",
+                   "高雄":"Kaohsiung",
+                   "新北":"New Taipei",
+                   "宜蘭":"Yilan",
+                   "桃園":"Taoyuan",
+                   "嘉義":"Chiayi",
+                   "新竹":"Hsinchu",
+                   "苗栗":"Miaoli",
+                   "南投":"Nantou",
+                   "彰化":"Changhua",
+                   "雲林":"Yunlin",
+                   "屏東":"Pingtung",
+                   "花蓮":"Hualien",
+                   "台東":"Taitung",
+                   "金門":"Kinmen",
+                   "澎湖":"Penghu",
+                   "連江":"Lienchiang"
+                   }
+                   
+def get_forecast_data(cityName):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
 
     response = requests.get(url=url)
 
     if response.ok:
         print("下載成功")
-        print(response.text)
+        return response.json()['list']
     else:
         print("下載失敗")
--- 專案3/main.py
+++ 專案3/main.py
@@ -1,8 +1,10 @@
-import datasource
+import datasource as ds
 
 def main():
     print("這裏是main function")
-    datasource.get_forecast_data()
+    list_data = ds.get_forecast_data(ds.tw_county_names["台北"])
+    for item in list_data:
+        print(item['dt_txt'])
 
 if __name__ == "__main__":
     print("這裏是程式的執行點")
